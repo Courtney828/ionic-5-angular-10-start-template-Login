a8be91e2de6b4dcd5973fb33dc39c450ada35ba3
Courtney828 <courtneyserongwa@gmail.com>
2020-09-21 06:38:03 -0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import{LoginComponent}from './login/login.component';
 import{HomeComponent}from './home/home.component';
 import{RegistrationComponent}from './registration/registration.component';
+import{ForgotpasswordComponent}from './forgotpassword/forgotpassword.component';
+
 
 const routes: Routes = [
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
   {path:'',component:HomeComponent},
   {path:'registation',component:RegistrationComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'Forgotpassword',component:ForgotpasswordComponent}
   
 
 ];
