ffaedf5170de65ca0a148cfaca899dfe76db5978
Courtney828 <courtneyserongwa@gmail.com>
2020-09-22 14:17:55 -0700

    2 files modified

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -9,261 +9,29 @@ import { AlertController } from 'ionic-angular';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
-   @ViewChild(List) list: List;
 
-  @ViewChild('sectionSelect') sectionSelect: Select;
-  newTask: string;
-  newTaskPriority: string;
-  items: [];
-  selecteOption: any;
-  noOfTasks: number;
-  constructor(private toastCtrl: ToastController, public alertCtrl: AlertController) { }
-  presentToast(statusText: string) {
-    let toast = this.toastCtrl.create({
-      message: statusText,
-      duration: 3000,
-      position: 'bottom'
-    });
-
-    toast.onDidDismiss(() => {
-      console.log('Dismissed toast');
-    });
-
-    toast.present();
-  }
-
-  showConfirm() {
-    let prompt = this.alertCtrl.create({
-      title: 'Edit',
-      message: "Update the task",
-      inputs: [
-        {
-          name: 'title',
-          placeholder: 'Title'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Save',
-          handler: data => {
-            console.log('data', data);
-            //this.newTask = data.title;
-            this.updateTask(data.title);
-            console.log('Saved clicked');
-          }
-        }
-      ]
-    });
-    prompt.present();
-  }
-
-
-  doFilter() {
-    this.sectionSelect.open();
-    console.log('hello')
-  }
-  ngOnInit() {
-    this.newTask = '';
-    this.newTaskPriority = 'vl';
-    this.items = [{
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "green"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "green"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "violet"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "violet"
-    },
-    {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "violet"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "yellow"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "yellow"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "yellow"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    },{
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "green"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "green"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "violet"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "violet"
-    },
-    {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "violet"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "yellow"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "yellow"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "yellow"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": false,
-      "priority": "pink"
-    }, {
-      "id": 0,
-      "content": "This is the task!!!",
-      "status": true,
-      "priority": "pink"
-    }];
-  this.noOfTasks = this.items.length;
-  }
-
-  itemSelected(i: number) {
-    this.selectedTask = i;
-    this.items[i].status = !this.items[i].status;
-    let statusText = (this.items[i].status) ? 'Task completed scuccessfully' : 'Task re-opened';
-    this.presentToast(statusText);
-  }
-
-  addTask(): void {
-    let newItem: any;
-    newItem = {
-      "id": this.items.length + 1,
-      "content": this.newTask,
-      "status": false,
-      "priority": this.newTaskPriority
-    };
-    this.items.unshift(newItem);
-    this.newTask = '';
-    this.presentToast('Task added scuccessfully');
-  }
-
-  taskPriorityChange(event, selecteOption): void {
-    //this.newTaskPriority = event;
-    switch(event){
-      case 'h': this.newTaskPriority = 'positive'; break;
-    }
-  }
-
-  updateTask(task:string, ):void{
-     this.items[this.selectedTask].content = task;
-     this.presentToast('Task updated scuccessfully');
-     this.noOfTasks = this.items.length;
-  }
-
-  dummy():void{
-    console.log("helloooooooooo");
-  }
-}
+ 
+taskName: any = ""; // Entered Text
+taskList = []; // Array to store tasks
+ 
+constructor() {}
  
 
+addTask() {
+if (this.taskName.length > 0) {
+let task = this.taskName;
+this.taskList.push(task);
+this.taskName = "";
+}
+}
 
+deleteTask(index) {
+this.taskList.splice(index, 1);
+}
+
+
+
+  ngOnInit() {
+  }
+
+}
